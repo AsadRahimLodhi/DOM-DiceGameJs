76b7c829389cce3c132df3e661b47efebfc4c2e2
Asad Rahim <asadlodhi166@gmail.com>
2022-09-17 06:13:20 +0500

    update

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6 + 1);
 
 document.querySelector('#current--' + activePlayer).textContent = dice;
+
+var x = document.querySelector('#current--0').textContent;
+
+console.log(x);
